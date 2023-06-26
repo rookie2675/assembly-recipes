ad35fa2948a2b88fba4c12b4f8edc5f4e67a9e3d
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-06-27 00:48:25 +0100

    Created a new class to configure repositories.

---
+++ IoC/ConfigureRepositories.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Repositories;
+using Repositories.Contracts;
+
+namespace IoC
+{
+    public static class ConfigureRepositories
+    {
+        public static void ConfigureWebAppRepositories(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            ArgumentNullException.ThrowIfNull(configuration);
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' is missing or null.");
+
+            serviceCollection.AddSingleton(connectionString);
+
+            serviceCollection.ConfigureCommonRepositories();
+        }
+
+        private static void ConfigureCommonRepositories(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IUserRepository, UserRepository>();
+            serviceCollection.AddScoped<IRecipeRepository, RecipeRepository>();
+        }
+    }
+}
--- IoC/ConfigureServices.cs
+++ IoC/ConfigureServices.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+
+using Services;
 using Services.Contracts;
 
 namespace IoC
 {
     public static class ConfigureServices
     {
-        public static void ConfigureWebApp(this IServiceCollection serviceCollection)
-        {
-            
-        }
+        public static void ConfigureWebAppServices(this IServiceCollection serviceCollection) => serviceCollection.ConfigureCommon();
 
         public static void ConfigureCommon(this IServiceCollection serviceCollection)
         {
-            // serviceCollection.AddScoped<IRecipeService, RecipeService>();
-        }
+            serviceCollection.AddScoped<IUserService, UserService>();
+            serviceCollection.AddScoped<IRecipeService, RecipeService>();
+            serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
+        } 
     }
 }
