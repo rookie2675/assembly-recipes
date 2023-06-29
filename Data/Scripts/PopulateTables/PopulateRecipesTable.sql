bcca6245335bcd17bf4026883c16bb7e8d36b41d
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-06-29 19:22:56 +0100

    Added more scripts to populate the tables and reorganized the data folder.

---
+++ Data/Scripts/PopulateTables/PopulateRecipesTable.sql
@@ -0,0 +1,22 @@
+INSERT INTO [dbo].[Recipes] ([Name], [Description], [ShortDescription])
+VALUES
+    ('Bacalhau à Brás', 'O Bacalhau à Brás é um prato típico da culinária portuguesa feito com bacalhau desfiado, batata palha, cebola, alho, azeitonas e ovos.', 'Um delicioso prato de bacalhau'),
+    ('Francesinha', 'A Francesinha é uma sanduíche típica do Porto, recheada com linguiça, salsicha fresca, fiambre, queijo e coberta com um molho especial à base de cerveja.', 'A iguaria mais famosa do Porto'),
+    ('Pastel de Nata', 'O Pastel de Nata é uma deliciosa sobremesa portuguesa composta por uma massa folhada crocante e um creme de ovos e baunilha.', 'Uma tentação irresistível'),
+    ('Caldo Verde', 'O Caldo Verde é uma sopa tradicional portuguesa feita com couve-galega, batata, cebola, alho e chouriço.', 'Uma sopa reconfortante'),
+    ('Arroz de Pato', 'O Arroz de Pato é um prato típico da gastronomia portuguesa que combina arroz, pato desfiado, chouriço, presunto e azeitonas.', 'Um prato rico em sabores'),
+    ('Polvo à Lagareiro', 'O Polvo à Lagareiro é um prato de polvo assado no forno com batatas, alho, azeite e acompanhado de grelos cozidos.', 'Uma especialidade marítima'),
+    ('Cozido à Portuguesa', 'O Cozido à Portuguesa é um prato tradicional português que combina carnes variadas, como carne de porco, vaca, frango, chouriço, com uma variedade de legumes.', 'Uma refeição completa'),
+    ('Bolo de Bolacha', 'O Bolo de Bolacha é uma sobremesa clássica em Portugal, feita com camadas de bolacha Maria embebidas em café e creme de manteiga e café.', 'Um doce irresistível'),
+    ('Arroz de Marisco', 'O Arroz de Marisco é um prato de arroz rico e saboroso preparado com uma seleção de mariscos frescos, como camarão, amêijoas, mexilhões e lagostins.', 'Uma explosão de sabores do mar'),
+    ('Queijadas de Sintra', 'As Queijadas de Sintra são pequenas tartes doces feitas com uma massa crocante e um recheio de queijo, ovos, açúcar e canela.', 'Uma delícia de Sintra'),
+    ('Arroz de Tamboril', 'O Arroz de Tamboril é um prato de arroz com tamboril, pimentos, tomate, cebola, alho e coentros.', 'Uma delícia do mar'),
+    ('Sardinhas Assadas', 'As Sardinhas Assadas são um prato típico do verão português, grelhadas e temperadas com sal grosso e servidas com pão e salada.', 'Sabores do mar'),
+    ('Feijoada à Transmontana', 'A Feijoada à Transmontana é um prato tradicional do norte de Portugal, feito com feijão, carne de porco, enchidos e legumes.', 'Sabor transmontano'),
+    ('Tripas à Moda do Porto', 'As Tripas à Moda do Porto são um prato emblemático da cidade do Porto, feito com tripas de porco, feijão, enchidos e outros ingredientes.', 'Uma herança histórica'),
+    ('Leitão à Bairrada', 'O Leitão à Bairrada é um prato famoso da região da Bairrada, caracterizado pelo leitão assado no forno com pele crocante e acompanhado de batatas fritas.', 'Sabor inigualável'),
+    ('Açorda Alentejana', 'A Açorda Alentejana é uma sopa tradicional alentejana feita com pão, alho, coentros, azeite e água quente, acompanhada de ovo escalfado e outros ingredientes.', 'Tradição alentejana'),
+    ('Alheira de Mirandela', 'A Alheira de Mirandela é um enchido tradicional de Mirandela, feito com carne de aves, pão, alho, azeite e especiarias.', 'Sabores únicos'),
+    ('Cataplana de Marisco', 'A Cataplana de Marisco é um prato típico do Algarve, preparado numa cataplana com mariscos, tomate, pimento, cebola, alho e especiarias.', 'Sabores do sul'),
+    ('Pastéis de Bacalhau', 'Os Pastéis de Bacalhau são bolinhos feitos com bacalhau desfiado, batata, cebola, salsa e fritos até ficarem dourados e crocantes.', 'Petisco irresistível'),
+    ('Tarte de Amêndoa', 'A Tarte de Amêndoa é uma sobremesa tradicional portuguesa feita com massa folhada crocante e um recheio de creme de amêndoa.', 'Doce tentação');
---
+++ Data/Scripts/PopulateTables/PopulateStetpsTable.sql
@@ -0,0 +1,284 @@
+-- Bacalhau à Brás Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (1, 'Desfie o bacalhau em lascas finas.'),
+    (1, 'Numa frigideira, aqueça o azeite e refogue a cebola e o alho.'),
+    (1, 'Adicione o bacalhau desfiado e deixe cozinhar por alguns minutos.'),
+    (1, 'Adicione a batata palha e envolva bem.'),
+    (1, 'Por último, adicione os ovos batidos e mexa rapidamente até os ovos cozinharem.');
+GO
+
+-- Francesinha Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (2, 'Preaqueça o forno a 180°C.'),
+    (2, 'Numa frigideira, frite as linguiças e as salsichas frescas até ficarem douradas.'),
+    (2, 'Em uma fatia de pão, coloque uma fatia de fiambre e uma fatia de queijo.'),
+    (2, 'Coloque a linguiça e a salsicha frita em cima do queijo.'),
+    (2, 'Cubra com outra fatia de pão e pressione levemente.'),
+    (2, 'Prepare o molho misturando todos os ingredientes e aquecendo em fogo baixo até ferver.'),
+    (2, 'Coloque a sanduíche em um prato e despeje o molho por cima.'),
+    (2, 'Leve ao forno por alguns minutos para gratinar.'),
+    (2, 'Sirva quente e desfrute!');
+GO
+
+-- Pastel de Nata Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (3, 'Preaqueça o forno a 220°C.'),
+    (3, 'Abra a massa folhada em uma superfície enfarinhada.'),
+    (3, 'Corte-a em quadrados e forre as formas de empada.'),
+    (3, 'Em uma panela, misture o leite, o açúcar, a farinha, as gemas e a baunilha.'),
+    (3, 'Leve ao fogo médio, mexendo sempre, até engrossar.'),
+    (3, 'Coloque o creme nas formas forradas com massa.'),
+    (3, 'Leve ao forno por cerca de 15 minutos ou até dourar.'),
+    (3, 'Retire do forno e deixe esfriar.'),
+    (3, 'Polvilhe com canela em pó e sirva!');
+GO
+
+-- Caldo Verde Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (4, 'Em uma panela, coloque as batatas e cubra com água.'),
+    (4, 'Cozinhe em fogo médio até as batatas ficarem macias.'),
+    (4, 'Escorra a água e reserve as batatas.'),
+    (4, 'Em outra panela, refogue a cebola e o alho em azeite.'),
+    (4, 'Adicione as batatas cozidas e amasse-as com um garfo.'),
+    (4, 'Acrescente água quente suficiente para obter a consistência desejada.'),
+    (4, 'Corte a couve-galega em tiras finas e adicione à panela.'),
+    (4, 'Deixe cozinhar por alguns minutos até a couve ficar macia.'),
+    (4, 'Tempere com sal e pimenta a gosto.'),
+    (4, 'Sirva quente com um fio de azeite.');
+GO
+
+-- Arroz de Pato Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (5, 'Cozinhe o pato em água com sal até ficar macio.'),
+    (5, 'Retire o pato da água, desfie a carne e reserve.'),
+    (5, 'Em uma panela, refogue a cebola e o alho em azeite.'),
+    (5, 'Adicione o chouriço em rodelas e deixe dourar.'),
+    (5, 'Acrescente o presunto picado e refogue por mais alguns minutos.'),
+    (5, 'Adicione o pato desfiado e misture bem.'),
+    (5, 'Em outra panela, cozinhe o arroz em água com sal até ficar al dente.'),
+    (5, 'Escorra o arroz e adicione à panela com o pato.'),
+    (5, 'Envolva bem e deixe cozinhar por alguns minutos para os sabores se misturarem.'),
+    (5, 'Sirva quente e desfrute!');
+GO
+
+-- Polvo à Lagareiro Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (6, 'Pré-aqueça o forno a 200°C.'),
+    (6, 'Cozinhe o polvo em água com sal até ficar macio.'),
+    (6, 'Escorra o polvo e coloque-o em uma assadeira.'),
+    (6, 'Regue o polvo com azeite e tempere com sal e alho picado.'),
+    (6, 'Leve ao forno por cerca de 30 minutos, virando o polvo na metade do tempo.'),
+    (6, 'Enquanto isso, cozinhe as batatas em água com sal até ficarem macias.'),
+    (6, 'Escorra as batatas e amasse-as levemente.'),
+    (6, 'Em uma frigideira, aqueça azeite e alho picado.'),
+    (6, 'Adicione as batatas amassadas e frite até dourarem.'),
+    (6, 'Retire o polvo do forno e sirva com as batatas fritas e grelos cozidos.');
+GO
+
+-- Cozido à Portuguesa Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (7, 'Em uma panela grande, coloque água e leve ao fogo alto.'),
+    (7, 'Adicione as carnes variadas (porco, vaca, frango), chouriço e enchidos.'),
+    (7, 'Deixe cozinhar em fogo médio por cerca de 1 hora até as carnes ficarem macias.'),
+    (7, 'Adicione os legumes (batata, cenoura, repolho, nabo, feijão-verde) e cozinhe por mais 30 minutos.'),
+    (7, 'Retire as carnes e os legumes da panela e disponha-os em um prato.'),
+    (7, 'Corte as carnes em fatias e sirva com os legumes e o caldo quente.'),
+    (7, 'Acompanhe com arroz branco e desfrute!');
+GO
+
+-- Bolo de Bolacha Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (8, 'Prepare o café forte e deixe esfriar.'),
+    (8, 'Num prato fundo, mergulhe rapidamente as bolachas no café e coloque-as no fundo de uma forma retangular.'),
+    (8, 'Bata a manteiga e o açúcar até obter uma mistura cremosa.'),
+    (8, 'Adicione as gemas e continue batendo.'),
+    (8, 'Acrescente o café solúvel dissolvido em um pouco de água morna e misture bem.'),
+    (8, 'Bata as claras em neve e incorpore delicadamente à mistura.'),
+    (8, 'Despeje metade do creme sobre as bolachas no fundo da forma.'),
+    (8, 'Faça outra camada de bolachas embebidas em café e cubra com o restante do creme.'),
+    (8, 'Alise a superfície e leve à geladeira por algumas horas até ficar firme.'),
+    (8, 'Desenforme e decore com bolacha moída por cima.'),
+    (8, 'Sirva gelado e delicie-se!');
+GO
+
+-- Arroz de Marisco Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (9, 'Em uma panela grande, aqueça azeite e refogue a cebola e o alho até ficarem dourados.'),
+    (9, 'Adicione os tomates picados e cozinhe por alguns minutos até amolecerem.'),
+    (9, 'Acrescente o pimentão, o louro, o vinho branco e deixe ferver por alguns minutos para evaporar o álcool.'),
+    (9, 'Adicione os mariscos (camarão, amêijoas, mexilhões, lagostins) e mexa bem.'),
+    (9, 'Tempere com sal, pimenta e pimenta-caiena a gosto.'),
+    (9, 'Em outra panela, cozinhe o arroz em água com sal até ficar al dente.'),
+    (9, 'Escorra o arroz e adicione à panela com os mariscos.'),
+    (9, 'Envolva bem e cozinhe por mais alguns minutos para os sabores se fundirem.'),
+    (9, 'Sirva quente e aproveite a explosão de sabores do mar!');
+GO
+
+-- Queijadas de Sintra Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (10, 'Pré-aqueça o forno a 180°C.'),
+    (10, 'Em uma tigela, misture o queijo fresco, os ovos, o açúcar e a farinha de trigo até obter uma massa homogênea.'),
+    (10, 'Adicione a manteiga derretida e continue misturando.'),
+    (10, 'Unte forminhas de queijada com manteiga e polvilhe com farinha de trigo.'),
+    (10, 'Despeje a massa nas forminhas, preenchendo cerca de 3/4 da capacidade.'),
+    (10, 'Leve ao forno por aproximadamente 25 minutos ou até dourarem e firmarem.'),
+    (10, 'Retire do forno e deixe esfriar antes de desenformar.'),
+    (10, 'Polvilhe as queijadas com açúcar e canela antes de servir.'),
+    (10, 'Desfrute dessas delícias de Sintra!');
+GO
+
+    -- Arroz de Pato Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (11, 'Cozinhe o pato em água com sal até ficar macio.'),
+    (11, 'Retire o pato da água e desfie a carne.'),
+    (11, 'Em uma panela, aqueça o azeite e refogue a cebola e o alho até dourarem.'),
+    (11, 'Adicione o chouriço em rodelas e o presunto cortado em cubos e refogue por alguns minutos.'),
+    (11, 'Acrescente o pato desfiado e misture bem.'),
+    (11, 'Adicione o arroz e envolva-o na mistura.'),
+    (11, 'Regue com caldo de carne suficiente para cozinhar o arroz.'),
+    (11, 'Tempere com sal, pimenta e cominhos a gosto.'),
+    (11, 'Cozinhe em fogo baixo até o arroz ficar macio e absorver o líquido.'),
+    (11, 'Sirva quente e desfrute dessa deliciosa combinação de sabores!');
+GO
+
+-- Arroz Doce Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (12, 'Em uma panela, coloque o arroz e água suficiente para cobri-lo.'),
+    (12, 'Cozinhe o arroz em fogo baixo até ficar macio e a água ser absorvida.'),
+    (12, 'Adicione o leite, o açúcar, a casca de limão, a canela em pau e o sal.'),
+    (12, 'Cozinhe em fogo médio, mexendo sempre, até obter uma consistência cremosa.'),
+    (12, 'Retire a casca de limão e a canela em pau.'),
+    (12, 'Em uma tigela pequena, bata as gemas com um pouco do arroz doce.'),
+    (12, 'Adicione as gemas batidas ao arroz doce e mexa bem.'),
+    (12, 'Cozinhe por mais alguns minutos até o creme engrossar.'),
+    (12, 'Retire do fogo e deixe esfriar.'),
+    (12, 'Sirva em taças individuais polvilhado com canela em pó.');
+GO
+
+-- Cataplana de Marisco Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (13, 'Em uma cataplana (ou panela grande com tampa), aqueça o azeite.'),
+    (13, 'Refogue a cebola e o alho até dourarem.'),
+    (13, 'Adicione os tomates picados, o pimentão e o louro e cozinhe por alguns minutos.'),
+    (13, 'Acrescente os mariscos (camarão, mexilhões, amêijoas, lagostins) e o vinho branco.'),
+    (13, 'Tempere com sal, pimenta e pimenta-caiena a gosto.'),
+    (13, 'Tampe a cataplana e cozinhe em fogo médio por cerca de 10 minutos até os mariscos abrirem.'),
+    (13, 'Adicione o coentro picado e misture bem.'),
+    (13, 'Retire do fogo e sirva quente, acompanhado de pão para molhar no delicioso caldo.');
+GO
+
+-- Pudim Abade de Priscos Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (14, 'Pré-aqueça o forno a 180°C.'),
+    (14, 'Em uma tigela, misture o açúcar com a água e leve ao fogo até formar uma calda em ponto de fio.'),
+    (14, 'Retire a calda do fogo e deixe esfriar um pouco.'),
+    (14, 'Em outra tigela, bata as gemas com a calda de açúcar até obter um creme fofo e homogêneo.'),
+    (14, 'Adicione a banha de porco derretida, o vinho do Porto, a farinha de trigo, a canela em pó e o toucinho picado.'),
+    (14, 'Misture bem todos os ingredientes até obter uma massa lisa.'),
+    (14, 'Unte uma forma de pudim com caramelo e despeje a massa.'),
+    (14, 'Leve ao forno em banho-maria por aproximadamente 1 hora até o pudim ficar firme e dourado.'),
+    (14, 'Retire do forno e deixe esfriar antes de desenformar.'),
+    (14, 'Sirva o Pudim Abade de Priscos frio ou em temperatura ambiente.');
+GO
+
+-- Bifanas à Moda do Porto Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (15, 'Tempere as bifanas com sal, pimenta, alho picado e vinho branco.'),
+    (15, 'Deixe marinar por pelo menos 30 minutos.'),
+    (15, 'Aqueça uma frigideira grande e derreta a banha de porco.'),
+    (15, 'Adicione as bifanas e frite em fogo médio-alto até dourarem dos dois lados.'),
+    (15, 'Retire as bifanas da frigideira e reserve.'),
+    (15, 'Na mesma frigideira, adicione as rodelas de cebola e deixe cozinhar até ficarem macias e douradas.'),
+    (15, 'Volte as bifanas para a frigideira e misture bem com a cebola.'),
+    (15, 'Regue com um pouco de vinho branco e deixe cozinhar por mais alguns minutos para apurar o sabor.'),
+    (15, 'Retire do fogo e sirva as Bifanas à Moda do Porto quentes, acompanhadas de pão.'),
+    (15, 'Bom apetite!');
+GO
+
+-- Arroz de Marisco Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (16, 'Em uma panela grande, aqueça o azeite.'),
+    (16, 'Refogue a cebola e o alho até dourarem.'),
+    (16, 'Adicione o pimento e cozinhe por alguns minutos.'),
+    (16, 'Acrescente o tomate picado e cozinhe até começar a desfazer.'),
+    (16, 'Adicione o vinho branco e deixe cozinhar por mais alguns minutos para evaporar o álcool.'),
+    (16, 'Acrescente o caldo de marisco e deixe ferver.'),
+    (16, 'Tempere com sal, pimenta e piripiri a gosto.'),
+    (16, 'Adicione os mariscos (camarão, amêijoas, mexilhões, lagostins) e cozinhe até que os mariscos abram.'),
+    (16, 'Retire os mariscos da panela e reserve.'),
+    (16, 'Em uma panela separada, cozinhe o arroz em água com sal até ficar al dente.'),
+    (16, 'Escorra o arroz e adicione-o ao caldo de marisco, misturando bem.'),
+    (16, 'Adicione os mariscos reservados ao arroz e misture delicadamente.'),
+    (16, 'Sirva o Arroz de Marisco bem quente e desfrute das maravilhas do mar!');
+GO
+
+-- Queijadas de Sintra Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (17, 'Pré-aqueça o forno a 180°C.'),
+    (17, 'Forre as formas para queijadas com a massa folhada.'),
+    (17, 'Em uma tigela, misture o queijo fresco, os ovos, o açúcar e a farinha de trigo.'),
+    (17, 'Bata tudo muito bem até obter uma massa homogênea.'),
+    (17, 'Divida a massa pelas formas preparadas.'),
+    (17, 'Leve as queijadas ao forno e deixe assar por cerca de 20-25 minutos, até dourarem.'),
+    (17, 'Retire do forno e deixe esfriar antes de desenformar.'),
+    (17, 'Polvilhe as Queijadas de Sintra com açúcar em pó e canela antes de servir.'),
+    (17, 'Saboreie essas delícias doces típicas de Sintra!');
+GO
+
+-- Pastéis de Bacalhau Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (18, 'Coloque o bacalhau em uma panela com água e leve ao fogo médio para cozinhar até ficar macio.'),
+    (18, 'Escorra o bacalhau, retire a pele e as espinhas e desfie a carne.'),
+    (18, 'Em uma tigela, misture a batata cozida e amassada, o bacalhau desfiado, a cebola picada, os ovos, a salsa picada e tempere com sal e pimenta a gosto.'),
+    (18, 'Amasse todos os ingredientes até obter uma massa homogênea.'),
+    (18, 'Aqueça uma frigideira com óleo e, com o auxílio de duas colheres, molde os pastéis de bacalhau.'),
+    (18, 'Frite os pastéis em óleo quente até ficarem dourados e crocantes.'),
+    (18, 'Retire os pastéis com uma escumadeira e coloque-os em papel absorvente para retirar o excesso de óleo.'),
+    (18, 'Sirva os Pastéis de Bacalhau como petisco ou acompanhamento de uma refeição.'),
+    (18, 'Aproveite essa delícia portuguesa!');
+GO
+
+-- Pudim Molotov Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (19, 'Pré-aqueça o forno a 180°C.'),
+    (19, 'Bata as claras em neve bem firme.'),
+    (19, 'Adicione o açúcar aos poucos, continuando a bater até obter um merengue brilhante.'),
+    (19, 'Adicione a baunilha e o vinagre branco e misture delicadamente.'),
+    (19, 'Forre uma forma de pudim com caramelo e despeje a massa de claras.'),
+    (19, 'Leve ao forno em banho-maria por aproximadamente 30-40 minutos, até o pudim ficar dourado e firme.'),
+    (19, 'Desligue o forno e deixe o pudim esfriar completamente dentro do forno.'),
+    (19, 'Desenforme o Pudim Molotov e sirva gelado.'),
+    (19, 'Desfrute dessa sobremesa leve e delicada!');
+GO
+
+-- Arroz Doce Steps
+INSERT INTO RecipeSteps (RecipeId, Step)
+VALUES
+    (20, 'Em uma panela, leve ao fogo o leite com o arroz e a casca de limão, mexendo ocasionalmente.'),
+    (20, 'Quando o arroz estiver cozido e o leite tiver reduzido, adicione o açúcar e mexa bem até dissolver completamente.'),
+    (20, 'Em uma tigela separada, bata as gemas e adicione um pouco do leite quente, mexendo sempre para temperar as gemas.'),
+    (20, 'Adicione a mistura de gemas à panela, mexendo vigorosamente para evitar que as gemas cozinhem e formem grumos.'),
+    (20, 'Cozinhe por mais alguns minutos até o arroz doce engrossar.'),
+    (20, 'Retire a casca de limão e distribua o arroz doce em tigelas individuais.'),
+    (20, 'Polvilhe com canela em pó a gosto.'),
+    (20, 'Deixe esfriar antes de servir.'),
+    (20, 'Delicie-se com esse clássico doce português!');
