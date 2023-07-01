02275653c92f259551442cd89d769e195989a0c6
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-01 01:20:52 +0100

    Added valdiations to the user properties.

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,11 +1,17 @@
-﻿namespace Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
 {
     public class User
     {
-        public required ushort Id { get; set; }
+        public long? Id { get; set; }
 
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username length must be between 1 and 50 characters.")]
         public required string Username { get; set; }
 
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "Password length must be between 1 and 100 characters.")]
         public required string Password { get; set; }
     }
 }
