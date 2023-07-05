e858bc362a900861ae4f8b6bf0cb5bc7d9e63aa2
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-05 20:03:47 +0100

    The create users table script now deletes any table with the same name and added auto-generated identity.

--- Data/Scripts/CreateTables/CreateUsersTable.sql
+++ Data/Scripts/CreateTables/CreateUsersTable.sql
@@ -1,11 +1,17 @@
+-- Drop the table if it already exists
+IF OBJECT_ID('Users', 'U') IS NOT NULL
+    DROP TABLE Users;
+GO
+
+-- Create the Users table
 CREATE TABLE Users
 (
-    Id SMALLINT NOT NULL,
+    Id SMALLINT IDENTITY(1,1) NOT NULL,
     Username NVARCHAR(50) NOT NULL,
     Password NVARCHAR(100) NOT NULL,
+    Role NVARCHAR(20) NOT NULL,
     CONSTRAINT PK_User PRIMARY KEY (Id),
     CONSTRAINT UK_User_Username UNIQUE (Username),
-    CONSTRAINT CK_User_IdRange CHECK (Id >= 0 AND Id <= 65535),
     CONSTRAINT CK_User_UsernameLength CHECK (LEN(Username) >= 1 AND LEN(Username) <= 50),
     CONSTRAINT CK_User_PasswordLength CHECK (LEN(Password) >= 1 AND LEN(Password) <= 100)
-);  
+);
