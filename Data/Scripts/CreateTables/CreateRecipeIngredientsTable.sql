2c439fbc55713362d83ddeea62cb142c8d648733
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-12 00:19:32 +0100

    Added 'NOT NULL' for the recipe id's and limited the size of the ingredient name to 20.

--- Data/Scripts/CreateTables/CreateRecipeIngredientsTable.sql
+++ Data/Scripts/CreateTables/CreateRecipeIngredientsTable.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE RecipeIngredients
 (
-    RecipeId INT,
-    Ingredient NVARCHAR(100) NOT NULL,
+    RecipeId BIGINT NOT NULL,
+    Ingredient NVARCHAR(20) NOT NULL,
     CONSTRAINT FK_RecipeIngredients_Recipes FOREIGN KEY (RecipeId) REFERENCES Recipes(Id) ON DELETE CASCADE
 );
