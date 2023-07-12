9350a4719e324e632b4cc20b6b4a74fdc24b84ab
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-12 22:50:21 +0100

    Removed annotation validations and added role property.

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,21 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Domain
+﻿namespace Domain
 {
     public class User
     {
         public long? Id { get; set; }
 
-        [Required(ErrorMessage = "Username is required.")]
-        [StringLength(50, ErrorMessage = "Username length must be between 1 and 50 characters.")]
         public required string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, ErrorMessage = "Password length must be between 1 and 100 characters.")]
         public string? Password { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string? Email { get; set; }
+
+        public string? Role { get; set; }
     }
 }
