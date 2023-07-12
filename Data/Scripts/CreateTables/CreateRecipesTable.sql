3d848bb48ac59be7ed8d4e5337b377ca0df7d9f7
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-12 02:10:33 +0100

    Improved the 'create recipes' table.

--- Data/Scripts/CreateTables/CreateRecipesTable.sql
+++ Data/Scripts/CreateTables/CreateRecipesTable.sql
@@ -1,11 +1,7 @@
-﻿IF OBJECT_ID('Recipes', 'U') IS NOT NULL
-    DROP TABLE Recipes;
-GO
-
-CREATE TABLE Recipes
+﻿CREATE TABLE Recipes
 (
     Id BIGINT IDENTITY(1, 1) PRIMARY KEY,
-    Name NVARCHAR(100) NOT NULL,
+    Name NVARCHAR(65) NOT NULL,
     Description NVARCHAR(MAX),
     ShortDescription NVARCHAR(255),
     ImageURL NVARCHAR(255),
