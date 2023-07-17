6c8716a4510a889f4e479555a08d01119fe2a4c0
Ricardo Almeida <ricardo.almeida@students.assembly.pt>
2023-07-17 19:26:15 +0100

    Created profile page.

---
+++ WebApp/Pages/Profile.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Domain;
+using Services.Users;
+using System.Security.Claims;
+
+namespace WebApp.Pages
+{
+    public class UserProfileModel : PageModel
+    {
+        private readonly UserService _userService;
+
+        [BindProperty]
+        public User User { get; private set; }
+
+        public bool EditMode { get; private set; }
+
+        public UserProfileModel(UserService userService)
+        {
+            _userService = userService;
+            EditMode = false;
+        }
+
+        public IActionResult OnGet()
+        {
+            var user = HttpContext.User;
+            string userIdAsString = user.FindFirstValue(ClaimTypes.NameIdentifier);
+            long userId = long.Parse(userIdAsString);
+            User = _userService.GetById(userId);
+            return Page();
+        }
+
+        public IActionResult OnPostEditProfile()
+        {
+            EditMode = true;
+            return Page();
+        }
+
+        public IActionResult OnPostCancelEdit()
+        {
+            EditMode = false;
+            User = _userService.GetById(User.Id.Value);
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _userService.Update(User);
+            EditMode = false;
+            User = _userService.GetById(User.Id.Value);
+
+            return Page();
+        }
+    }
+}
