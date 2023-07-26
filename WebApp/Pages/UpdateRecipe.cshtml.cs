14a9cf9c54b1d49e88e8d21f04bc886396b296bb
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-07-26 20:19:35 +0100

    Created razor controller for the update recipe page.

--- WebApp/Pages/UpdateRecipe.cshtml.cs
+++ WebApp/Pages/UpdateRecipe.cshtml.cs
@@ -1,12 +1,48 @@
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Recipes;
 
 namespace WebApp.Pages
 {
     public class UpdateRecipeModel : PageModel
     {
-        public void OnGet()
+        private readonly IRecipeService _recipeService;
+        private readonly ILogger<UpdateRecipeModel> _logger;
+
+        [BindProperty]
+        public Recipe? Recipe { get; set; }
+
+        public UpdateRecipeModel(ILogger<UpdateRecipeModel> logger, IRecipeService recipeService)
         {
+            _logger = logger;
+            _recipeService = recipeService;
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (!id.HasValue)
+                return NotFound();
+
+            Recipe = _recipeService.GetById(id.Value);
+
+            if (Recipe is null)
+            {
+                _logger.LogInformation($"No recipe was found for ID: {id}");
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            _recipeService.Update(Recipe);
+
+            return RedirectToPage("/Recipes");
         }
     }
 }
