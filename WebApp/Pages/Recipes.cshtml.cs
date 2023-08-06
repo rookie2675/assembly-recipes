bcf20b68271179702984d61c120159867ca9f766
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-08-06 18:09:48 +0100

    Removed pagination since it was broken. Now fetch all recipes.

--- WebApp/Pages/Recipes.cshtml.cs
+++ WebApp/Pages/Recipes.cshtml.cs
@@ -9,34 +9,11 @@ namespace WebApp.Pages
         private readonly IRecipeService _recipeService;
 
         public required IEnumerable<Recipe> Recipes { get; set; }
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-        public bool IsAuthenticated { get; set; }
+
+        public bool IsUserAuthenticated => User?.Identity?.IsAuthenticated ?? false;
 
         public RecipesModel(IRecipeService recipeService) => _recipeService = recipeService;
 
-        public void OnGet(int page)
-        {
-            const int defaultPageSize = 9;
-            int pageSize = defaultPageSize;
-
-            if (page <= 0)
-                page = 1;
-
-            int totalRecipes = _recipeService.GetTotalCount();
-            TotalPages = CalculateTotalPages(totalRecipes, pageSize);
-
-            if (page > TotalPages)
-                page = TotalPages;
-
-            CurrentPage = page;
-            Recipes = _recipeService.GetPage(CurrentPage, pageSize);
-        }
-
-        private static int CalculateTotalPages(int totalItems, int pageSize)
-        {
-            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
-            return totalPages > 0 ? totalPages : 1;
-        }
+        public void OnGet() => Recipes = _recipeService.GetAll();
     }
 }
